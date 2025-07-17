5105572066827d52f478c36cecebd14649d86f3d
Zuzana Piarova <zpiarova@c2r9s7.42prague.com>
2025-07-17 15:42:55 +0200

    need to change the structure cpp04/ex00

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -11,4 +11,17 @@ int main(void)
     a.makeSound();
     c.makeSound();
     d.makeSound();
+
+    // !!!!!!!!!111
+    // const Animal* meta = new Animal();
+    // const Animal* j = new Dog();
+    // const Animal* i = new Cat();
+    // std::cout << j->getType() << " " << std::endl;
+    // std::cout << i->getType() << " " << std::endl;
+    // i->makeSound(); //will output the cat sound!
+    // j->makeSound();
+    // meta->makeSound();
+    // ...
+
+    return 0;
 }
