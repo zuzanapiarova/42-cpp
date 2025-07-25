425c0d0ba71b971302950f6c9b6f41d8d4588ded
Zuzana Piarova <zpiarova@c2r10s7.42prague.com>
2025-07-25 15:00:52 +0200

    started cpp06

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,22 @@
+#include "ScalarConverter.hpp"
+
+void ScalarConverter::convert( std::string s)
+{
+    // std::cout << "String: " <<  s << std::endl;
+    // std::cout << "Char: " <<  static_cast<char>(s) << std::endl;
+    // std::cout << "Int: " <<  s << std::endl;
+    // std::cout << "Float: " <<  s << std::endl;
+    // std::cout << "Double: " <<  s << std::endl;
+
+//     int i = std::stoi(input);
+// 🔹 String to float
+// cpp
+// Copy
+// Edit
+// float f = std::stof(input);
+// 🔹 String to double
+// cpp
+// Copy
+// Edit
+// double d = std::stod(input);
+}
---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,17 @@
+#ifndef SCALAR_CONVERTER_H
+# define SCALAR_CONVERTER_H
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter
+{
+    private:
+        ScalarConverter();
+
+    public:
+        static void convert( std::string s);
+
+};
+
+#endif
